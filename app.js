712088f0bf527389a8ca57bff33fc2555708f59e
thanayut-sae <thanayut.saeng@gmail.com>
2026-04-01 18:42:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ================================================================
 //  CONFIG
 // ================================================================
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbyMOHEKSdL47F-b_fy514eWwgisCFLDQs9Gss2_inZE6-MKjg0PsOdBKZtjUGrulqM2/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxt33j5opIc-o_Hi-anO668VBxagnMdJO26K8DuCjSxltsC0MwgOTFj6QbTErgGuId-/exec';
 
 // ================================================================
 //  STATE
